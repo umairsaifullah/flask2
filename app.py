3fd50cf056617593200827806eb5777fddafd893
umairsaifullah <45719817+umairsaifullah@users.noreply.github.com>
2023-03-23 06:24:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def submit():
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-  app.run(port=5000)
+    app.run(debug=True)
+  #app.run(port=5000)
